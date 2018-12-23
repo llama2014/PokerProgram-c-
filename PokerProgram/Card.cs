77eed44fb129d04141696c935f7aef7186c357e4
theconstantone <theconstantllama@gmail.com>
2018-12-23 13:14:47 -0800

    Add project files.

---
+++ PokerProgram/Card.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Poker
+{
+
+    using System;
+
+    internal class Card
+    {
+        public Card() : this(new Random()) { }
+
+        public Card(Random r)
+        {
+            Number = r.Next(2, 15);
+            Suit = r.Next(1, 5);
+            Name = numberArray[Number - 2] + suitArray[Suit - 1];
+
+        }
+
+        public Card(string name)
+        {
+            string n = name.Substring(0, 1);
+            string s = name.Substring(1, 1);
+            Number = numberString.IndexOf(n) + 2;
+            Suit = suitString.IndexOf(s) + 1;
+            Name = name;
+
+        }
+
+
+
+        public readonly int Number;
+        public readonly int Suit;
+        public readonly string Name;
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            try
+            {
+                Card c = (Card)obj;
+                return c.Number == Number && c.Suit == Suit;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return (Suit << 4) + Number;
+
+        }
+
+        // beginning of private fields
+
+        private static string[] numberArray = { "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A" };
+
+        private static string[] suitArray = { "C", "D", "H", "S" };
+
+        private static string numberString = "23456789TJQKA";
+
+        private static string suitString = "CDHS";
+    }
+
+}
---
+++ PokerProgram/Hand.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    //using Poker;
+    using System;
+
+    public class Hand
+    {
+        public Hand()
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                while (true)
+                {
+                    cards[i] = new Card(r);
+                    if (containedCard(cards[i], cards, i)) continue;
+                    break;
+
+                }
+            }
+        }
+
+        public Hand(string handText)
+        {
+            cardsFromString(handText);
+        }
+
+        public Hand(string handText, string holdString)
+        {
+            cardsFromString(handText);
+            holdCards(holdString);
+            draw();
+        }
+
+        public Hand(Hand hand, string holdString)
+        {
+            this.cards = hands.cards;
+            holdCards(holdString);
+            draw();
+        }
+
+        public int Score
+        {
+            get
+            {
+                if (score < 0) calcScore();
+                return score;
+
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return titles[Score];
+            }
+
+        }
+
+        public string cardName(int cardNum)
+        {
+            return cards[cardNum - 1].Name;
+        }
+
+
+    }
+}
